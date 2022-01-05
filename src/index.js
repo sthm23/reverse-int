5b8279c4d08e80398413122deb9fde8dd4ec44d0
Tukhtamishev Sanjar <stuxtamixhev@gmail.com>
2022-01-05 17:49:05 +0500

    fix: complate func reverse num

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+
+    let str_n = Math.abs(n).toString();
+    let arr = [];
+    let arr1 = [];
+    
+
+    for(let i=0; i<str_n.length; i++){
+        arr.push(str_n[i]);
+        
+    }
+    for(let j=0; j<arr.length; j++){
+        let a = arr.length-1;
+        arr1[j] = arr[a-j];
+    }
+  return arr1.join('');
 }
